b7697253e5bf93256a8bcc2098670c780d92e2e0
Dennis Nesanoff <nndennisnn@gmail.com>
2020-01-01 20:38:12 +0300

    Initial Commit

---
+++ UIActivityViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIActivityViewController
+//
+//  Created by Dennis Nesanoff on 01.01.2020.
+//  Copyright © 2020 Dennis Nesanoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
